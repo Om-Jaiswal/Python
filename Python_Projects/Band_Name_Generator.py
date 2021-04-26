13a7128d412ede7206c9956dffe4c8edfbda25b3
Om Jaiswal <74256880+Om-Jaiswal@users.noreply.github.com>
2021-04-26 22:57:52 +0530

    Add files via upload

---
+++ Python_Projects/Band_Name_Generator.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator!")
+
+city = input("Which city did you grow up in?\n")
+pet_name = input("What is the name of a pet?\n")
+
+print("Your Band Name could be " + city + " " + pet_name)
