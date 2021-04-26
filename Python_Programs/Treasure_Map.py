8095a8998d47cd4e448a3bc16d0432137025ff36
Om Jaiswal <74256880+Om-Jaiswal@users.noreply.github.com>
2021-04-27 02:03:56 +0530

    Update Treasure_Map.py

--- Python_Programs/Treasure_Map.py
+++ Python_Programs/Treasure_Map.py
@@ -9,6 +9,7 @@ position = input("Where do you want to put the treasure?\n")
 horizontal = int(position[0])
 vertical = int(position[1])
 
+# map[vertical-1][horizontal-1]="X"
 selected_row = map[vertical - 1]
 selected_row[horizontal - 1] = "X"
 
