14133ebd61c41c14eb4d2f68e5f8fc5a2b5ceda2
Om Jaiswal <74256880+Om-Jaiswal@users.noreply.github.com>
2021-04-27 02:02:01 +0530

    Update FizzBuzz.py

--- Python_Programs/FizzBuzz.py
+++ Python_Programs/FizzBuzz.py
@@ -1,9 +1,23 @@
+# for number in range(1,101):
+#   if number % 3 == 0 and number % 5 == 0:
+#     print("FizzBuzz")
+#   elif number % 3 == 0:
+#     print("Fizz")
+#   elif number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print(number)
+    
+print("Welcome to FizzBuzz!")
+Fizz = int(input("Enter a number for fizz :\n"))
+Buzz = int(input("Enter a number for buzz :\n"))
+print("Here's the solution")
 for number in range(1,101):
-  if number % 3 == 0 and number % 5 == 0:
-    print("FizzBuzz")
-  elif number % 3 == 0:
-    print("Fizz")
-  elif number % 5 == 0:
-    print("Buzz")
-  else:
-    print(number)
+    if number % Fizz == 0 and number % Buzz == 0:
+        print("FizzBuzz")
+    elif number % Fizz == 0:
+        print("Fizz")
+    elif number % Buzz == 0:
+        print("Buzz")
+    else:
+        print(number)
