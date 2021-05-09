0904286a840cc8e207ee8819c9a63c84eb56581f
Om Jaiswal <74256880+Om-Jaiswal@users.noreply.github.com>
2021-05-09 20:29:34 +0530

    Update main.py

--- Python_Programs_OPP/Comprehension/main.py
+++ Python_Programs_OPP/Comprehension/main.py
@@ -40,27 +40,3 @@ weather_c = {
 weather_f = {day:(temp_c*9/5) + 32 for (day, temp_c) in weather_c.items()}
 
 print(weather_f)
-
-# Pandas
-import pandas
-
-student_dict = {
-  "student": ["Om", "Parag", "Shantanu"],
-  "score": [98, 76, 56]
-}
-
-# Loop through dictionaries
-# for (key, value) in student_dict.items():
-#   print(value)
-
-student_data_frame = pandas.DataFrame(student_dict)
-print(student_data_frame)
-
-# Loop through a data frame
-# for (key, value) in student_data_frame.items():
-#   print(value)
-
-# Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-  if row.student == "Om":
-    print(row.score)
